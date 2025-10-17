154a799ea24906b7a412169097abbca2d33a67b7
Yury Krutikov <yura7714@mail.ru>
2025-10-17 17:38:10 +0500

    Rename controller

--- src/main/java/ru/krutikov/MySpringBoot2Dbase/controller/StudentController.java
+++ src/main/java/ru/krutikov/MySpringBoot2Dbase/controller/StudentController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-public class MyController {
+public class StudentController {
     @Autowired
     private StudentService studentService;
 
